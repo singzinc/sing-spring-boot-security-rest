36e5e2fe8800fdf613e71dbe859489190d4ea8b6
chunsinglau <chunsinglau@10.0.0.107>
2017-03-09 11:58:07 +0800

    the first version

---
+++ sing-spring-boot-security-rest/src/main/java/com/singplayground/application/Application.java
@@ -0,0 +1,96 @@
+package com.singplayground.application;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.singplayground.model.Account;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    /*
+    @Bean
+    CommandLineRunner init(final AccountRepository accountRepository) {
+      
+      return new CommandLineRunner() {
+
+        @Override
+        public void run(String... arg0) throws Exception {
+          accountRepository.save(new Account("rbaxter", "password"));
+          
+        }
+        
+      };
+
+    }
+    */
+    
+}
+
+@Configuration
+class WebSecurityConfiguration extends GlobalAuthenticationConfigurerAdapter {
+
+  //@Autowired
+  //AccountRepository accountRepository;
+
+  @Override
+  public void init(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userDetailsService());
+  }
+
+  @Bean
+  UserDetailsService userDetailsService() {
+    return new UserDetailsService() {
+
+      @Override
+      public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+       // Account account = accountRepository.findByUsername(username);
+       System.out.println("XXXXXXXXXX ");
+       Account account = new Account(); 
+       account.setId("12345");
+       account.setPassword("password");
+       account.setUsername("username1");
+      if(account != null) {
+        return new User(account.getUsername(), account.getPassword(), true, true, true, true,
+                AuthorityUtils.createAuthorityList("USER"));
+        } else {
+          throw new UsernameNotFoundException("could not find the user '"
+                  + username + "'");
+        }
+      }
+      
+    };
+  }
+}
+
+@EnableWebSecurity
+@Configuration
+class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+ 
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests().anyRequest().fullyAuthenticated().and().
+    httpBasic().and().
+    csrf().disable();
+  }
+  
+}
